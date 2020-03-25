e52dac1be05597df29d67967a6550882a7a3ceff
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-03-25 03:39:44 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 #write your code here
 
 def countdown(number)
-  while number > 0 
-    puts "#{number} SECOND(S)!"
+  do puts "#{number} SECOND(S)!"
     number = number -1
+  until number ==0
   end
     
 end
