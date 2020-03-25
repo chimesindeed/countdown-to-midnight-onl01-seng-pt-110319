5d412e91d3951adc8da635d52de99ed98b3233ef
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-03-25 03:48:10 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,13 @@
 #write your code here
 
 def countdown(number)
-  do puts "#{number} SECOND(S)!"
+  output = "#{number} SECOND(S)!"
+  until number == 0
+    puts output
     number = number -1
-  until number ==0
-end
-    
+  end
+  if number == 0
+    output = "Happy New Year!"
+  end
+  output
 end
